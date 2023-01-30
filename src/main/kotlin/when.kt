b9f87476c7e180ea57f0f1432bd8bf38bdd66403
mattkeyzer <mkeyzer282@students.mchenry.edu>
2023-01-30 12:10:39 -0600

    When

---
+++ src/main/kotlin/when.kt
@@ -0,0 +1,29 @@
+fun main(args: Array<String>) {
+    println("Let's learn some Spanish...")
+    println("Enter a number 1 - 10")
+    val number = readLine()!!.toInt()
+    if (number !in 1..10)
+        println("Please try again!")
+
+    else {
+        var numString = "x"
+
+        when (number){
+            1 -> numString = "Uno"
+            2 -> numString = "Dos"
+            3 -> numString = "Tres"
+            4 -> numString = "Cuatro"
+            5 -> numString = "Cinco"
+            6 -> numString = "Seis"
+            7 -> numString = "Siete"
+            8 -> numString = "Ocho"
+            9 -> numString = "Nueve"
+            10 -> numString = "Diez"
+            !in 1..10 -> numString = "Out of Range"
+            else -> numString = "Unknown"
+        }
+
+        println("$number in spanish is $numString")
+
+    }
+}
