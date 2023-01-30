3928b26bff589da98a8b92b35d6810a2d2bf52e2
mattkeyzer <105663113+mattkeyzer@users.noreply.github.com>
2023-01-29 19:36:18 -0600

    Looping

---
+++ src/main/kotlin/looping.kt
@@ -0,0 +1,28 @@
+fun main() {
+    var userin = 0
+
+    do {
+        println()
+        println("Welcome to Matt's Marvelous Diner...")
+        println("What do you fancy?")
+        println()
+        println("1. Hamburger")
+        println("2. Cheeseburger")
+        println("3. BLT")
+        println("4. Panini")
+        println("5. Grilled Cheese")
+        println("6. EXIT")
+        userin = readLine()!!.toInt()
+
+        if (userin in 1..5 )
+            println()
+            println("yay")
+    }
+
+
+
+     while(
+         userin != 6
+    )
+
+}
