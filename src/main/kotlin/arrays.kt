265f42bdf187832f1e765df91540a30858c717f1
mattkeyzer <mkeyzer282@students.mchenry.edu>
2023-02-06 10:45:08 -0600

    Arrays

---
+++ src/main/kotlin/arrays.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    var icecreamArray = arrayOf("vanilla ice cream", "chocolate ice cream", "sprinkles", "chocolate syrup", "cherries")
+    for (singlePart in icecreamArray) {
+        println(singlePart)
+    }
+}
